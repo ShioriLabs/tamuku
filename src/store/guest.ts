a1ae7642f0191664d2732f6234cb6b96afd246a3
Dhika Rizky <deka_r@outlook.com>
2021-02-07 04:23:28 +0700

    Added organizationsAtom atom and some count atoms

--- src/store/guest.ts
+++ src/store/guest.ts
@@ -11,6 +11,19 @@ export interface Guest {
 
 export const guestsAtom = atom<Guest[]>([])
 
+export const organizationsAtom = atom<{organization: string, count: number }[]>(
+  get => get(guestsAtom)
+    .filter(guest => guest.organization !== '')
+    .map(guest => guest.organization)
+    .reduce((prev, item) => {
+      let newArray = [...prev]
+      const isExistsIndex = newArray.findIndex(arrItem => arrItem.organization === item)
+      if (isExistsIndex > -1) newArray[isExistsIndex].count = newArray[isExistsIndex].count + 1
+      else newArray = [...newArray, { organization: item?.toString() ?? '', count: 1 }]
+      return newArray
+    }, [] as {organization: string, count: number }[])
+)
+
 export const modifyGuestsAtom = atom<Guest[], Guest[]>(
   get => get(guestsAtom),
   async (_get, set, newList): Promise<void> => {
@@ -23,6 +36,10 @@ export const addGuestAtom = atom<Guest[], Guest>(
   get => get(modifyGuestsAtom),
   (get, set, newGuest) => set(modifyGuestsAtom, [...get(modifyGuestsAtom), newGuest])
 )
+
+export const getGuestsCount = atom(get => get(guestsAtom).length)
+
+export const getOrganizationsCount = atom(get => get(organizationsAtom).length)
 
 export const createNewGuest = (name: string, organization?: string | null): Guest => {
   return {
